2539756af04146f7331628ce579efbbb5bce9e58
susannejung <95688233+susannejung@users.noreply.github.com>
2022-01-15 08:52:12 +0100

    Initial commit

---
+++ src/FilEksempler/Main.java
@@ -0,0 +1,10 @@
+package FilEksempler;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("1. udgave");
+        System.out.print("2. udgave");
+    }
+}
